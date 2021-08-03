d682031a6e986afe2ba5c9d7f48a81b85f6efd45
Smurfcatcher <82717311+Smurfcatcher@users.noreply.github.com>
2021-08-03 06:42:57 -0400

    commit

---
+++ javascript-getting-started/02/demos/after/js-demo/Home.js
@@ -0,0 +1,25 @@
+// Show the title
+let price = 49.99,
+    name = 'Hiking Boots',
+    Hello = "Hello \"World\"",
+    here = name.length,
+    discounted = false;
+
+let persons = {
+    Firstname: 'John',
+    Lastname: 'Futral'
+};
+
+price++;
+
+showMessage(persons.Lastname);
+
+//showMessage(typeof name);
+//showMessage("Title...");
+console.log(price);
+console.log(Hello)
+
+/*
+    Detail complex logic...
+    some algorithm...
+ */
