71ee7bfea3a92ac785a140afa31e647b1bca5c10
Smurfcatcher <82717311+Smurfcatcher@users.noreply.github.com>
2022-01-31 15:23:00 -0500

    Commit
    commit

---
+++ script.js
@@ -0,0 +1,41 @@
+//---------------------------
+// alert("Hello World");
+// alert("Access Granted");
+// let welcome = 'welcome',
+//     product = 149.99,
+//     discount = True;
+
+//---------------------------
+// showmessage(welcome);
+
+// // showmessage("Title Added");
+
+// // showmessage("Title 2");
+
+//---------------------------
+// console.log("any message...");
+
+// let product = 'hiking boots';
+
+// console.log(product);
+
+
+//---------------------------
+// let times99 = 99;
+// showmessage(times99);
+
+//---------------------------
+// let price;
+// price = 99.99;
+// showmessage(price);
+
+//---------------------------
+// let price = 20.00,
+//     taxrate = 0.07;
+// price += 5;
+// showmessage(typeof price);
+// showmessage(++price);
+
+//---------------------------
+let hello = "Hello \"World\"";
+showmessage(hello);
